8e4fd315aa52018576361c83043f509944bb8001
adnfalll___ <118400004+adnanzainal21@users.noreply.github.com>
2023-03-26 16:12:15 +0700

    Perlu NISN

--- c++/inpo.cpp
+++ c++/inpo.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main() {
 
     string nama=" ADNAN ZAINAL ARIFIN";
+    string nisn="0061960371";
     int umur= 18;
     string jenis_kelamin="LAKI LAKI";
     string alamat="JATISRONO WONOGIRI";
 
     cout << "=====BIODATA=====" << endl;
     cout << "nama" << nama << endl ;
+    cout << "NISN" << nisn << endl ;
     cout << "umur" << umur << endl ;
     cout << " jenis kelamin" << jenis_kelamin << endl ;
     cout << " alamat " << alamat << endl ;
